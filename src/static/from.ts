34b4f15fc64e2ff1e7226e7c486bdf16d77df1df
Patrick Roberts <git@patrickroberts.dev>
2021-12-22 05:56:43 -0600

    Updated dependencies, dropped support for Node v10

--- src/static/from.ts
+++ src/static/from.ts
@@ -5,7 +5,7 @@ const from: {
   (z: Complex): Complex;
   (real: number, imag?: number): Complex;
   (...args: [z: Complex] | [real: number, imag?: number]): Complex;
-} = (r: Complex | number, i = 0): Complex => {
+} = (r: Complex | number, i: number = 0): Complex => {
   if (typeof r === 'number') {
     return new Complex(r, i, 0, 0, Component.CARTESIAN);
   }
